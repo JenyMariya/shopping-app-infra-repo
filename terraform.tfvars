ecd481f0a6dbfaf931c8b5c45703716f6534b9fa
jeny <jeny@gmail.com>
2023-12-29 08:10:16 +0000

    infra initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+project_name = "shopping_app"
+project_env = "prod"
+domain_name = "mikhaelnoah.online"
+hostname = "shopping"
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "instance_type" {}
+variable "ami_id" {}
+variable "project_name" {}
+variable "project_env" {}
+variable "domain_name" {}
+variable "hostname" {}
